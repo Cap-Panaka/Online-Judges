636f71e2664d8b1eec0e05e5dde3e8ebd910a8e0
Cap-Panaka <nerdcrab1122@gmail.com>
2025-08-09 01:18:05 +0600

    Create 50-50 Rule (Codechef).cpp

---
+++ 50-50 Rule (Codechef).cpp
@@ -0,0 +1,22 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int t;
+    cin>>t;
+    while(t--){
+    int x,y;
+    cin>>x>>y;
+    if(x<50)
+    {
+        cout<<"Z"<<'\n';
+    }
+    else if(y<50)
+    {
+        cout<<"F"<<'\n';
+    }
+    else{
+        cout<<"A"<<'\n';
+    }
+    } 
+}
