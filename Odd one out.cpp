6310f16cbc1e9217980ad1ad4b5436032049b668
Cap-Panaka <nerdcrab1122@gmail.com>
2025-08-14 23:03:50 +0600

    Create Odd one out.cpp

---
+++ Odd one out.cpp
@@ -0,0 +1,24 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main() 
+{
+    int t;
+    cin>>t;
+    while(t--)
+    {
+        int a,b,c;
+        cin>>a>>b>>c;
+        if(a==b)
+        {
+            cout<<c<<'\n';
+        }
+        else if(b==c)
+        {
+            cout<<a<<'\n';
+        }
+        else if(a==c)
+        {
+            cout<<b<<'\n';
+        }
+}
+}
