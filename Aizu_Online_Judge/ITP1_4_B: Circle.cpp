61cdca0d9c6e61b2d2555a356f0492d176cba14a
Cap-Panaka <nerdcrab1122@gmail.com>
2025-10-04 22:25:06 +0600

    Create ITP1_4_B: Circle.cpp

---
+++ Aizu_Online_Judge/ITP1_4_B: Circle.cpp
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include<math.h>
+int main() 
+{
+    double r;
+    double pi=acos(-1);
+    
+    scanf("%lf",&r);
+    printf("%.6lf %.6lf",pi*r*r,2*pi*r);
+}
+
+
+
+
+/*  XXX #include<stdio.h>
+//#include<math.h>
+int main()    //TEST CASE 3 XX
+{
+    double r, PI=3.14159265358979323846264338327950288419716939937510;
+    scanf("%lf",&r);
+    printf("%lf %lf",PI*r*r,2*PI*r);
+    
+}*/
