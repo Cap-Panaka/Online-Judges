e6dfa2c9a4a9f2a11f4ffe572771e09723c79a0f
Cap-Panaka <nerdcrab1122@gmail.com>
2025-10-05 16:19:07 +0600

    Create ITP1_1_C: Rectangle.cpp

---
+++ Aizu_Online_Judge/ITP1_1_C: Rectangle.cpp
@@ -0,0 +1,10 @@
+// https://judge.u-aizu.ac.jp/onlinejudge/description.jsp?id=ITP1_1_C
+
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    long long a,b;
+    cin>>a>>b;
+    cout<<a*b<<" "<<2*(a+b)<<"\n" ;
+}
