ab760013e3c22384afe282377cbe70c7b3d514bc
Cap-Panaka <nerdcrab1122@gmail.com>
2025-10-06 20:34:33 +0600

    Create A - Placing Marbles.c

---
+++ Atcoder/A - Placing Marbles.c
@@ -0,0 +1,10 @@
+// https://atcoder.jp/contests/abc081/tasks/abc081_a
+
+#include<stdio.h>
+int main()
+{
+    char a,b,c;
+    scanf("%c%c%c",&a,&b,&c);
+    int abc=(a-'0') + (b-'0') + (c-'0');
+    printf("%d",abc);
+}
