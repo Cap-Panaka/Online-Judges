036fb12be11ae9c8d57570749aaf7e52867786f7
Cap-Panaka <nerdcrab1122@gmail.com>
2025-10-08 09:55:06 +0600

    Create ITP1_3_A: Print Many Hello World.c

---
+++ Aizu_Online_Judge/ITP1_3_A: Print Many Hello World.c
@@ -0,0 +1,11 @@
+// https://judge.u-aizu.ac.jp/onlinejudge/description.jsp?id=ITP1_3_A
+
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    for(int i=0;i<1000;i++)
+    {
+        printf("Hello World\n");
+    }
+}
