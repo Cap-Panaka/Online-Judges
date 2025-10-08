8f97203d19843837ec37ace818ab39c8120f7084
Cap-Panaka <nerdcrab1122@gmail.com>
2025-10-08 18:19:32 +0600

    Create A - Long Loong.c

---
+++ Atcoder/A - Long Loong.c
@@ -0,0 +1,16 @@
+//  https://atcoder.jp/contests/abc336/tasks/abc336_a
+
+#include<stdio.h>
+int main()
+{
+int n;
+scanf("%d",&n);
+printf("L");
+for(int i=0;i<n;i++)
+{
+    printf("o");
+}
+printf("ng");
+
+
+}
