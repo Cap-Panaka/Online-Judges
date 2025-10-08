73c6e2baff263f3aee148b012177b62144fea0e9
Cap-Panaka <nerdcrab1122@gmail.com>
2025-10-08 18:40:28 +0600

    Create A - Not Too Hard.cpp

---
+++ Atcoder/A - Not Too Hard.cpp
@@ -0,0 +1,21 @@
+// https://atcoder.jp/contests/abc328/tasks/abc328_a
+
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int t,n,x,sum=0;
+    cin>>t>>n;
+    while(t--)
+    {
+        //int x;
+        cin>>x;
+        if(x<=n)
+        {
+            sum=sum+x;
+        }
+        
+       
+    }
+     cout<<sum;
+}
