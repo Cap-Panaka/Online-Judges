3a8aa882bc86a00315355816da00fd8081487f6e
Cap-Panaka <nerdcrab1122@gmail.com>
2025-10-15 14:58:04 +0600

    Update B - Multi Test Cases.c

--- Atcoder/B - Multi Test Cases.c
+++ Atcoder/B - Multi Test Cases.c
@@ -1,5 +1,37 @@
 // https://atcoder.jp/contests/abc284/tasks/abc284_b
 
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    
+    int t;
+    
+    cin>>t;
+    while(t--)
+    {
+        int n;
+        cin>>n;
+        int i,ar[n];
+        for(i=0;i<n;i++)
+        {
+            cin>>ar[i];
+        }
+        int count=0;
+        for(i=0;i<n;i++)
+        {
+            if(ar[i]%2!=0)
+            {
+                count++;
+            }
+        }
+        cout<<count<<'\n';
+        
+    }
+}
+
+
+/*
 #include<stdio.h>
 int main()
 {
@@ -25,3 +57,4 @@ int main()
         printf("%d\n",count);
     }
 }
+ */
