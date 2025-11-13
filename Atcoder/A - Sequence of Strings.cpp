d475d25bc73fef295862d047cbc10770448ee666
Cap-Panaka <nerdcrab1122@gmail.com>
2025-11-13 15:04:22 +0600

    Implement string reversal for input sequence

--- Atcoder/A - Sequence of Strings.cpp
+++ Atcoder/A - Sequence of Strings.cpp
@@ -1,6 +1,33 @@
 // https://atcoder.jp/contests/abc284/tasks/abc284_a
+// https://atcoder.jp/contests/abc284/tasks/abc284_a
 
 #include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int n;
+    cin>>n;
+    vector<string>s;
+    
+    for(int i=0;i<n;i++)
+    {
+        string temp;
+        cin>>temp;
+        s.push_back(temp);
+    
+    }
+    
+    reverse(s.begin(),s.end());
+    for(int i=0;i<n;i++)
+    {
+        cout<<s[i]<<'\n';
+    }
+    
+}
+
+//or 
+
+/*#include<bits/stdc++.h>
 using namespace std;
 int main()
 {
@@ -17,4 +44,4 @@ int main()
         cout<<s[i]<<"\n";
     }
     
-}
+} */
