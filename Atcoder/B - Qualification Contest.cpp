b58bad3922b784180cc3ff13f35aa848ab0b979c
Cap-Panaka <nerdcrab1122@gmail.com>
2025-11-14 11:26:33 +0600

    Fix loop condition in Qualification Contest.cpp

---
+++ Atcoder/B - Qualification Contest.cpp
@@ -0,0 +1,21 @@
+// https://atcoder.jp/contests/abc288/tasks/abc288_b
+
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int n,k;
+    cin>>n>>k;
+    vector<string>v(n);
+    for(int i=0;i<n;i++)
+    {
+        cin>>v[i];
+    }
+    
+    sort(v.begin(),v.begin()+k);
+    
+    for( int i=0;i<k;i++)
+    {
+        cout<<v[i]<<"\n";
+    }
+}
